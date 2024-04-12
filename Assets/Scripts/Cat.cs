aeb2f24110065a89941a4d4fef4588e3a087360e
Francef <France_fortin1@hotmail.com>
2024-04-11 22:14:25 -0700

    Set up animations and Navmesh agents to have cats follow player within a particular radius. Otherwise cycle between lookaround, idle, and patrol states

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -1,14 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Cat : MonoBehaviour
 {
+    public float IdleTime { get; private set; } = 2.0f;         // time to spend in idle state
+    public float FollowRange { get; private set; } = 5.0f;      // when player is closer than this, follow
+    public float WaitRange { get; private set; } = 1.0f;    // when player is closer than this, wait
     bool isFollowing = false;
-    // Start is called before the first frame update
+
+    public GameObject Player { get; private set; }
+    public NavMeshAgent Agent { get; private set; }
+    public List<Vector3> Waypoints { get; private set; }      // waypoints for patrol state
+    private int waypointIndex = 0;                              // current waypoint index
+    private Vector3 initialCatPos;
+    private Vector3 wayPoint1;
+    private Vector3 wayPoint2;
+    private bool hasJustPatrolled;
     void Start()
     {
-        
+        Agent = GetComponent<NavMeshAgent>();                   // get a reference to the NavMeshAgent
+        Player = GameObject.FindGameObjectWithTag("Player");    // get a reference to the Player
+
+        // Create and populate a list of waypoints
+        Waypoints = new List<Vector3>();
+        initialCatPos = transform.position;
+        // set initial waypoint to 2 points away from initial position on the z axis
+        wayPoint1 = new Vector3(initialCatPos.x, initialCatPos.y, initialCatPos.z - 2);
+        Waypoints.Add(wayPoint1);
+        // set second waypoint to 2 points away from initial position on the z axis
+        wayPoint2 = new Vector3(initialCatPos.x, initialCatPos.y, initialCatPos.z + 2);
+        Waypoints.Add(wayPoint2);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Draw a sphere to show follow range in Scene
+        Gizmos.DrawWireSphere(transform.position, FollowRange);
+    }
+
+    public float GetDistanceFromPlayer()
+    {
+        //Get the distance(in units) from the cat to the player
+        return Vector3.Distance(transform.position, Player.transform.position);
+    }
+
+    public void DetermineNextWaypoint()
+    {
+        waypointIndex++;
+        if (waypointIndex >= Waypoints.Count)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    public Vector3 GetCurrentWaypoint()
+    {
+        //return the current waypoint
+        return Waypoints[waypointIndex];
+    }
+
+    public bool isTimeToLookAround()
+    {
+        return hasJustPatrolled;
+    }
+
+    public void SetHasJustPatrolled(bool patrolStatus)
+    {
+        hasJustPatrolled = patrolStatus;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/CatPatrolState.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPatrolState : CatStateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        cat.DetermineNextWaypoint();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 currWaypoint = cat.GetCurrentWaypoint();
+        cat.transform.LookAt(currWaypoint);   // look at next waypoint, prevent appearance of gliding
+        cat.Agent.SetDestination(currWaypoint);
+        if (cat.Agent.pathPending) { return; }
+        if (cat.Agent.remainingDistance <= cat.Agent.stoppingDistance)
+        {
+            cat.SetHasJustPatrolled(true);  // tell cat script that patrol was just finished
+            animator.SetTrigger("idle"); // go to idle state
+        }
+        else if (cat.GetDistanceFromPlayer() < cat.FollowRange)
+        {
+            animator.SetTrigger("follow");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
